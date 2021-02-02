80fc8092437b562d32efb71ad5074a65ddd6099f
Patricia Faustino <patriciafaustinobr@gmail.com>
2021-02-02 20:17:25 -0300

    Exercícios dos topicos do capitulo 3

---
+++ tiposValoresVariaveis.js/numeros/aritmetica01.js
@@ -0,0 +1,47 @@
+/** Em JS temos os operadores arítmeticos que são 
+ * +,-,/,*,%
+ */
+
+ /**Como facilitador pode-se ter resultados de algumas
+  * operações com o objeto Math
+  */
+
+// Potência
+  console.log(Math.pow(2,53))
+
+// Arredondamento para o inteiro mais próximo
+console.log(Math.round(.6))
+
+// Arredondamento para cima para o inteiro
+console.log(Math.ceil(.6))
+
+// Arredondamento para baixo para o inteiro
+console.log(Math.floor(.6))
+
+// Valor absoluto
+console.log(Math.abs(-5))
+
+// Retorna o maior argumento
+let x = 10
+let y = 5
+let z = 15
+
+console.log(Math.max(x, y, z))
+
+// Retorna o menor argumento
+console.log(Math.min(x, y, z))
+
+// Número pseudoaleatório x, onde x está entre 0 e 1.0
+console.log(Math.random())
+
+// Circunferência de um círculo
+console.log(Math.PI)
+
+// Número de Euler ou a base do logaritmo natural
+console.log(Math.E)
+
+// Raiz quadrada
+console.log(Math.sqrt(3))
+
+// Raiz de forma a escolher o índice
+console.log(Math.pow(3, 1/3))
---
+++ tiposValoresVariaveis.js/numeros/literaisInteiros.js
@@ -0,0 +1,27 @@
+/*
+ * Js não faz distinção entre valores inteiros 
+ * e valores em ponto flutuante
+ */
+
+ // Literais inteiros de base 10
+ 0
+ 3
+ 1000
+
+ /*
+ * Literais hexadecimais
+ * Começa com 0x ou 0X e é seguido por uma sequência de
+ * letras de a(ou A) até f(F) e números de 0 a 9
+ */
+
+ 0xff // 15 * 16 + 15 = 255
+ 0XCAFE911
+ -0xF1A7
+
+ /**Literal octal
+  * começa com 0 e é seguido por uma sequência de dígitos que
+  * vai de 0 a 7
+  *  */ 
+
+ 0377 // 3 * 64 + 7 * 8 = 255
+ -077 
